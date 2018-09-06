c352c1fdf2b3e2c6320476fd3c65ec2ca0827729
Ahmed Hamdy <dimo.hamdy@gmail.com>
2018-09-06 05:13:35 +0200

    get files
    add files API to get json have all uploaded files and add feature of return json of created object when uplaod file

--- final code/routes/user/index.js
+++ final code/routes/user/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var url = 'mongodb://localhost:27017/myproject';
+const path = require('path');
 
 var multer = require('multer');
 var storage = multer.diskStorage({
@@ -10,7 +11,7 @@ var storage = multer.diskStorage({
       cb(null, 'public/images/uploads')
     },
     filename: (req, file, cb) => {
-      cb(null, file.fieldname + '-' + Date.now())
+      cb(null, file.fieldname + '-' + Date.now() +  path.extname(file.originalname));
     }
 });
 var upload = multer({storage: storage});
@@ -18,19 +19,55 @@ var upload = multer({storage: storage});
 router.post('/fileUpload', upload.single('image'), (req, res, next) => {
     MongoClient.connect(url, (err, db) => {
         assert.equal(null, err);
-        insertDocuments(db, 'public/images/uploads/' + req.file.filename, () => {
+        var filePath = '/images/' + req.file.filename /*+  path.extname(req.file.originalname)*/;
+        insertDocuments(db, filePath, (result) => {
             db.close();
-            res.json({'message': 'File uploaded successfully'});
+            res.json(result/*{'path': filePath }*/);
         });
     });
 });
+
+
+router.get('/files', function (req, res) {
+  MongoClient.connect(url, (err, db) => {
+      assert.equal(null, err);
+      getDocuments(db, (result) => {
+          db.close();
+          res.json(result);
+      });
+  });
+});
+
 
 module.exports = router;
 
 var insertDocuments = function(db, filePath, callback) {
     var collection = db.collection('user');
     collection.insertOne({'imagePath' : filePath }, (err, result) => {
-        assert.equal(err, null);
-        callback(result);
+        if(err) {
+     console.log('Error occurred while inserting');
+     assert.equal(err, null);
+    // return
+ } else {
+    console.log('inserted record', result.ops[0]);
+   // return
+   callback(result.ops[0]);
+
+ }
     });
-}
+
+    collection.all
+};
+
+
+var getDocuments = function(db, callback) {
+  var collection = db.collection('user');
+
+  collection.find({}).toArray(function(err, result) {
+  //if (err) throw err;
+  console.log(result);
+  assert.equal(err, null);
+  callback(result);
+});
+    collection.all
+};
